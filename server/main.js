bdb685d22485034a3dc804d775b930c0e5d47542
Manuel Morini <morinimanuel@gmail.com>
2015-08-26 18:11:34 +0200

    missing file for server enhancements

--- server/main.js
+++ server/main.js
@@ -3,13 +3,17 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var config = require('./lib/config');
 
 var routes = require('./routes/index');
+var video = require('./routes/video');
 
 var app = express();
 
-app.set('port', process.env.PORT || 3000);
-//app.use(multer({ dest: 'uploads/'}))
+//app.set('port', process.env.PORT || 3000);
+
+
+
 app.set('views', __dirname + '/views'); // general config
 app.set('view engine', 'html');
 
@@ -29,39 +33,43 @@ app.use(function(req, res, next) {
 
 
 app.use('/', routes);
+app.use('/videos', video);
 
-// catch 404 and forward to error handler
+
+
+/// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-// error handlers
+/// error handlers
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.json( {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
-app.listen(app.get('port'));
 
+app.listen(process.env.PORT || config.port);
+console.log("Listening on port: " + (process.env.PORT || config.port));
 
 module.exports = app;
